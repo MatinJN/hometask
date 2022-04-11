49ee7c6ec91d285ecc17b98d0a741d49dd2fbf12
MeteHan96 <matinjn@code.edu.az>
2022-04-12 02:50:28 +0400

    Proje dosyası ekle.

---
+++ Home Work/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Home_Work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("UserName daxil edin");
+            string username = Console.ReadLine();
+            Console.WriteLine("password daxil edin");
+            string password = Console.ReadLine();
+            User user = new User(username);
+            user.Username = username;
+            user.Password = password;
+            
+        }
+    }
+}
---
+++ Home Work/User.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Home_Work
+{
+    internal class User
+    {
+        private string _userName;
+        private string _password;
+
+        public string Username
+        {
+            get 
+            {
+                return _userName; 
+            }
+            set
+            {
+              if (value.Length > 8)
+              {
+
+                _userName = value;
+
+              }
+                Console.WriteLine("Ay Yuzer 8 dene knopka bas hec olmasa)))");
+            }
+        }
+        public User(string user)
+        {
+            Username = user;
+        }
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                int count = 0;
+                int count1 = 0;
+                int count2 = 0;
+                if (value.Length >= 8)
+                {
+
+                foreach (char item in value)
+                {
+
+                if (char.IsDigit(item))
+                {
+                  count++;
+
+                }
+                else if (char.IsLetter(item))
+                { 
+                  count1++;
+                }
+                else if (char.IsDigit(item))
+                 {
+
+                  count2++;
+                        
+                 }
+                        if (count && count1 && count2 > 0 )
+                        {
+
+                        }
+
+                _password = value;
+                }
+                }
+            }
+        }
+    }
+}
